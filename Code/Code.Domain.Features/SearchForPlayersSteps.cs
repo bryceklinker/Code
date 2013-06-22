a4e8f978b0e7b93732b9a9c8cb8d71bdb8a4dc05
bryceklinker <bryce.klinker@gmail.com>
2013-06-22 14:14:54 -0500

    Adding spec flow and first feature file.

---
+++ Code/Code.Domain.Features/SearchForPlayersSteps.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace Code.Domain.Features
+{
+    [Binding]
+    public class SearchForPlayersSteps
+    {
+        [Given(@"a set of players")]
+        public void GiveASetOfPlayers()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"a player with name (.*)")]
+        public void GivenAPlayerWithName(string name)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I enter (.*) as name")]
+        public void WhenIEnterName(string name)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I search")]
+        public void WhenISearch()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I enter potential (.*)")]
+        public void WhenIEnterPotential(int potential)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I enter current ability (.*)")]
+        public void WhenIEnterCurrentAbility(int currentAbility)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I enter position (.*)")]
+        public void WhenIEnterPosition(string position)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I enter age (.*)")]
+        public void WhenIEnterAge(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"I get (.*) in results")]
+        public void ThenIGetInResults(string name)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"I get every player with potential greater than or equal to (.*)")]
+        public void ThenIGetEveryPlayerWithPotentialGreaterThanOrEqualTo(int potential)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"I get every player with current ability greater than or equal to (.*)")]
+        public void ThenIGetEveryPlayerWithCurrentAbilityGreaterThanOrEqualTo(int currentAbility)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"I get every player with position (.*)")]
+        public void ThenIGetEveryPlayerWithPosition(string position)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"I get every player with age less than or equal to (.*)")]
+        public void ThenIGetEveryPlayerWithAgeLessThanOrEqualTo(int age)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
---
+++ Code/Code.Domain.Features/SearchPlayers.feature
@@ -0,0 +1,31 @@
+﻿Feature: Search for players
+
+Background:
+	Given a set of players
+
+@mytag
+Scenario: Search for players by name
+	Given a player with name Neymar
+	When I enter Ney as name
+	And I search
+	Then I get Neymar in results
+
+Scenario: Search for players by potential
+	When I enter potential 100
+	And I search
+	Then I get every player with potential greater than or equal to 100
+
+Scenario: Search for players by current ability
+	When I enter current ability 100
+	And I search
+	Then I get every player with current ability greater than or equal to 100
+
+Scenario: Search for players by position
+	When I enter position central midfield
+	And I search
+	Then I get every player with position central midfield
+
+Scenario: Search for players by age
+	When I enter age 20
+	And I search
+	Then I get every player with age less than or equal to 20
